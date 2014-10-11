3c612b6c20a43fa54adbd8c564d61b0a150c90c3
Jake S <tud45372@temple.edu>
2014-10-11 12:11:46 -0400

    Got more information out of the API, but the steps of the router are still in a list with 1 index

--- testing_gmaps_module.py
+++ testing_gmaps_module.py
@@ -3,15 +3,29 @@ import json
 from parse import *
 api = Directions()
  
-results = api.directions("Temple University,  Philadelphia PA","2296 Byecroft Rd, New Hope PA")
+results = api.directions("Temple University,  Philadelphia PA","2296 Byecroft Rd, New Hope PA", "walking")
+#place json response into dictionary
+dict_results = json.loads(json.dumps(results,sort_keys=True))[0]
+#place legs (where distance, duration, and waypoints are) into dict
+legs_dict = json.loads(json.dumps(dict_results['legs'],sort_keys=True))[0]
+#place back into master dict
+dict_results['legs']=legs_dict
+
+
+#ONLY RETURNS ONE LEG DOESNT WORK
+#place the steps into dictionary 
+steps_dict = json.loads(json.dumps(dict_results['legs']['steps'],sort_keys=True))[0]
+
+#print dict_results
 #print json.dumps(results, sort_keys=True, indent = 4, separators=("'",': '))
+#print json.dumps(dict_results['legs'], sort_keys=True, indent = 2, separators=("'",': '))
 
 #test of input files library
 dest_zip_file = 'data/merck_work_zip'
 commuter_file = 'data/merck_zip_data'
 #get zip codes of destinations 
 dest_zip = get_dest_zip(dest_zip_file)
-
+print dest_zip
 #get list of all commuters
 commuters = get_commuter(commuter_file)
-print commuters
+#print commuters
