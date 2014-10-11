444c7206397da25faf6371c472475591befaa2bf
Jake S <tud45372@temple.edu>
2014-10-10 22:01:56 -0400

    Added comments in parse.py

--- parse.py
+++ parse.py
@@ -4,8 +4,13 @@
 
 #Library to parse the input files we are given
 
-#assumes file looks like: <name> is <zipcode>
 def get_dest_zip(dest_zip_file):
+	"""
+	Function to get the destation zip codes out of the given files
+	Takes input file destination 
+	Assumes input file looks like: <name> is <zipcode>
+	Returns dictionary of [Name of Place][Zip]
+	"""
 	dest_zip_file_contents = open (dest_zip_file)
 
 	for line in dest_zip_file_contents:
@@ -14,11 +19,6 @@ def get_dest_zip(dest_zip_file):
 		dest_zip = {line_split[0]:line_split[1]}
 		return dest_zip
 
-#assumes commute file has 3 columns and a single line header
-#col1 is ID number
-#col2 is home zip code
-#col3 is Org	
-#returns dictionary with [Zip][Org][Id]	
 def get_commuter(commuter_file):
 	"""
 	Function to get all commuters from input file commuter_file
