0a815eff00aad0c9ac7793cd71c0529424e07ceb
Jake S <tud45372@temple.edu>
2014-10-14 22:00:34 -0400

    code to collect traffic data works

--- collect_traffic_data.py
+++ collect_traffic_data.py
@@ -10,7 +10,6 @@ lat_1=-76
 lon_2=42
 
 bing = BingTraffic()
-s = sched.scheduler(time.time, time.sleep)
 
 def run_day():
 	now = datetime.datetime.now()
@@ -18,28 +17,27 @@ def run_day():
 	#Run forever
 	while True:
 		#Run everyday during 4-7pm rush hour
-		if stime < datetime.time(16,0):
-			stime = datetime.time(16,0)
-			while stime <= datetime.time(19,0):
-				#run the program every 30 minutes
-				s.enterabs(stime,1, run_time, ())
-				now = datetime.datetime.now()
-				next_time = now + datetime.timedelta(seconds=1800)
-				stime = next_time.time() 
+		while stime > datetime.time(16,0) and stime <= datetime.time(19,0):
+			#run the program every 30 minutes
+			run_time()
+			time.sleep(1800)
+			now = datetime.datetime.now()
+			next_time = now + datetime.timedelta(seconds=1)
+			stime = next_time.time()
+				
 
 		#Run everyday during 7-10am rush hour
-		elif stime < datetime.time(7,0):
-			stime = datetime.time(7,0)
-			while stime <= datetime.time(19,0):
-				#run the program every 30 minutes
-				s.enterabs(stime,1, run_time, ())
-				now = datetime.datetime.now()
-				next_time = now + datetime.timedelta(seconds=1800)
-				stime = next_time.time()
+		while stime > datetime.time(7,0) and stime <= datetime.time(10,0):
+			#run the program every 30 minutes
+			run_time()
+			time.sleep(1800)
+			now = datetime.datetime.now()
+			next_time = now + datetime.timedelta(seconds=1800)
+			stime = next_time.time()
 
 def run_time():
-	response = 'jasdlf'
-	#response = bing.get_traffic(lon_1,lat_1,lon_2,lat_2)
+	#get bing response
+	response = bing.get_traffic(lon_1,lat_1,lon_2,lat_2)
 	#get current time
 	curr_time=time.time()
 	#cast time as string and remove the decimal point 
@@ -49,9 +47,9 @@ def run_time():
 	#open file and write date
 	with open(file_name, 'w+') as traffic_file:
 		traffic_file.write(str(curr_time)+'\n')
-		traffic_file.write(response)
+		traffic_file.write(json.dumps(response, sort_keys=True, indent=4, separators=("'",':')))
 
-#start program
+#run program
 run_day()
 
 
